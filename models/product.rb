27af208aecc478d2d3c9e22af14b49f1a5e1d110
lauren brett <laurencareybrett@gmail.com>
2019-12-13 14:33:42 +0000

    ye ye

--- models/product.rb
+++ models/product.rb
@@ -1,6 +1,12 @@
 require_relative('../db/sql_runner')
 
+class Product
 
-
-@buy_price = options['buy_price']
-@sell_price =
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @name = options['name']
+  @description = options['description']
+  @buy_price = options['buy_price']
+  @sell_price = options['sell_price']
+end
+end
